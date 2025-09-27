02c060565b5e97d19ea54905c84e11da0359894a
girapet <peter.h.girard@gmail.com>
2025-09-27 08:16:17 -0400

    fixed path for index.js

--- js/boot.js
+++ js/boot.js
@@ -1,7 +1,7 @@
   const host = new URL(document.location.href).hostname;
-  const isProduction = host === 'localhost';
+  const isProduction = host !== 'localhost';
   
   const script = document.createElement('script');
-  script.src = isProduction ? '/js/dist/index.js' : '/js/index.js';
+  script.src = isProduction ? 'js/dist/index.js' : 'js/index.js';
   script.type = 'module';
   document.body.appendChild(script);
